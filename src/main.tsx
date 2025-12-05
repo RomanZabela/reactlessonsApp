344f2df24afcff8b0533ab0263037c2884f15039
Roman Zabela <cplCold@gmail.com>
2025-12-05 09:57:13 +0200

    added react query and configure query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Home, About, NotFound } from './pages'
 import { createRoot } from 'react-dom/client'
+
+const queryClient = new QueryClient()
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,5 +21,9 @@ const router = createBrowserRouter(
 )
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <RouterProvider router={router} />
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 )
